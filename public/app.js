67a4442a3656a2371e52a11d8affaa4395f79ea1
Anandaram R <anandaramr1915@gmail.com>
2026-05-21 15:31:37 +0530

    Add more robust caching method for video previews

--- public/app.js
+++ public/app.js
@@ -2,41 +2,40 @@ let files = []
 let isShuffled = false
 let elementMap = new Map()
 
-const activeVideoCache = [];
-const MAX_CACHED_VIDEOS = 5; 
+const VIDEO_CACHE_TTL = 60 * 1000
+let cacheTTLMap = new Map()
 
-function manageVideoCache(newVid) {
-    const index = activeVideoCache.indexOf(newVid);
-    if (index !== -1) {
-        activeVideoCache.splice(index, 1);
-    }
-    
-    console.log('adding to cache')
-    activeVideoCache.push(newVid);
-    if (activeVideoCache.length > MAX_CACHED_VIDEOS) {
-        const oldestVid = activeVideoCache.shift();
-        console.log('removing from cache')
-        oldestVid.removeAttribute('src');
-        oldestVid.load(); 
-    }
+function onVideoPlay(fileId) {
+    clearTimeout(cacheTTLMap.get(fileId))
+    cacheTTLMap.delete(fileId)
+}
+
+function onVideoPause(fileId, vid) {
+    const timer = setTimeout(() => {
+        vid.removeAttribute('src')
+        vid.load()
+        cacheTTLMap.delete(fileId)
+    }, VIDEO_CACHE_TTL)
+
+    cacheTTLMap.set(fileId, timer)
 }
 
 init()
 
 async function init() {
-    const res = await fetch("/api/files")
+    const res = await fetch('/api/files')
     files = await res.json()
 
     params = new URLSearchParams(window.location.search)
-    isShuffled = params.has("shuffle")
+    isShuffled = params.has('shuffle')
     const fileList = isShuffled ? shuffleArray(files) : files
-    
+
     renderGrids(fileList)
     updateShuffledState(isShuffled)
 }
 
 function renderGrids(files) {
-    const grid = document.getElementById("grid")
+    const grid = document.getElementById('grid')
 
     for (let i = 0; i < files.length; i++) {
         const file = files[i]
@@ -44,77 +43,79 @@ function renderGrids(files) {
     }
 }
 
-const eventSource = new EventSource("/api/events")
+const eventSource = new EventSource('/api/events')
 
-eventSource.onmessage = (evt) => {
+eventSource.onmessage = evt => {
     const data = JSON.parse(evt.data)
-    const grid = document.getElementById("grid")
+    const grid = document.getElementById('grid')
 
-    if (data.action === "add") {
+    if (data.action === 'add') {
         insertGridItem(data.file, grid, true)
-    } else if (data.action === "remove") {
+    } else if (data.action === 'remove') {
         removeGridItem(data.file.id)
     }
 }
 
 function insertGridItem(file, grid, prepend = false) {
-    const preview = document.createElement("a")
+    const preview = document.createElement('a')
 
-    preview.className = "grid-item"
+    preview.className = 'grid-item'
     preview.href = `/api/files/${file.id}`
     preview.id = file.id
 
-    const media = document.createElement("img")
+    const media = document.createElement('img')
     media.src = `/api/thumb/${file.id}`
-    media.loading = "lazy"
-    media.className = file.type === "image" ? "img" : "video"
+    media.loading = 'lazy'
+    media.className = file.type === 'image' ? 'img' : 'video'
     preview.appendChild(media)
-    
-    if (file.type != "image") {
-        const icon = document.createElement("span")
-        icon.className = "play-icon"
-        
-        const iconImg = document.createElement("img")
-        iconImg.src = "/play_icon.svg"
-        
+
+    if (file.type != 'image') {
+        const icon = document.createElement('span')
+        icon.className = 'play-icon'
+
+        const iconImg = document.createElement('img')
+        iconImg.src = '/play_icon.svg'
+
         icon.appendChild(iconImg)
         preview.appendChild(icon)
 
         const vid = document.createElement('video')
-        vid.preload = "none"
-        vid.classList.add("video-preview", "fade")
+        vid.preload = 'none'
+        vid.classList.add('video-preview', 'fade')
         vid.muted = true
         vid.loop = true
-        
+
         preview.appendChild(vid)
-        
+
         preview.onmouseenter = () => {
-            if (window.matchMedia("(min-width: 769px)").matches) {
+            if (window.matchMedia('(min-width: 769px)').matches) {
                 if (!vid.src) {
                     vid.src = `/api/files/${file.id}`
                     vid.load()
                 }
-                
-                manageVideoCache(vid)
 
                 vid.classList.remove('fade')
                 media.classList.add('fade')
                 icon.classList.add('fade')
 
-                vid.play().catch(err => console.log("Play interrupted:", err))
+                onVideoPlay(file.id)
+                vid.currentTime = 0
+                vid.play().catch(err => console.log('Play interrupted:', err))
             }
         }
-        
+
         preview.onmouseleave = () => {
-            if (window.matchMedia("(min-width: 769px)").matches) {
+            if (window.matchMedia('(min-width: 769px)').matches) {
                 vid.pause()
+                onVideoPause(file.id, vid)
+
                 vid.classList.add('fade')
                 media.classList.remove('fade')
                 icon.classList.remove('fade')
             }
         }
     }
-    
+
     if (prepend) {
         grid.prepend(preview)
     } else {
@@ -126,10 +127,10 @@ function insertGridItem(file, grid, prepend = false) {
 function removeGridItem(fileId) {
     const child = elementMap.get(fileId)
     if (child) {
-        const grid = document.getElementById("grid")
+        const grid = document.getElementById('grid')
         grid.removeChild(child)
         elementMap.delete(child)
-        files = files.filter((f) => f.id !== fileId)
+        files = files.filter(f => f.id !== fileId)
     }
 }
 
@@ -137,7 +138,7 @@ function shuffleGrid() {
     const shuffledIds = shuffleArray(files.map(f => f.id))
     const grid = document.getElementById('grid')
     grid.replaceChildren(...shuffledIds.map(id => elementMap.get(id)).filter(Boolean))
-    
+
     updateShuffledState(true)
 }
 
@@ -151,7 +152,7 @@ function updateShuffledState(newState) {
     isShuffled = newState
     const button = document.getElementById('shuffle-btn')
     const url = new URL(window.location)
-    
+
     if (newState) {
         button.classList.add('active')
         button.classList.remove('inactive')
