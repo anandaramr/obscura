176da70ebc3b060d6ff6575222c9a592567a347e
Anandaram R <anandaramr1915@gmail.com>
2026-06-10 13:43:25 +0530

    Enable CORS to support custom clients

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import os from "os"
 import chokidar from "chokidar"
 import pLimit from "p-limit"
@@ -22,6 +23,7 @@ function createApp() {
     const app = express()
     app.use(express.json())
     app.use(logger())
+    app.use(cors())
     app.use(express.static(path.join(PROJECT_ROOT, "public")))
     return app
 }
